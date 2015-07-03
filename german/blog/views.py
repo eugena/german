58a885e73d10a539fb0bebf7a4838f39df92eed2
eugena <eugena.mihailikova@gmail.com>
2015-07-03 18:05:19 +0500

    refactor of like view

--- german/blog/views.py
+++ german/blog/views.py
@@ -1,25 +1,14 @@
-from django.http import HttpResponse
-from django.contrib.contenttypes.models import ContentType
-from django.template import loader
-from secretballot.views import vote
-from secretballot.models import Vote
+from secretballot.views import vote as vote_view
 from zinnia.models import Entry
 
 
-def like(request, id, update):
+def like(request, entry_id, vote):
     """
-    Like
+    Likes
     """
-    mimetype = 'application/json'
-    if update:
-         return vote(request, Entry, id, 1, can_vote_test=None,
-         redirect_url=None, template_name=None, template_loader=loader,
-         extra_context=None, context_processors=None, mimetype=mimetype)
-    else:
-        try:
-            votes = Vote.objects.filter(
-                content_type=ContentType.objects.get_for_model(Entry), object_id=id).count()
-            body = '{"num_votes": %d}' % votes
-        except:
-            body = '{"num_votes": ""}'
-        return HttpResponse(body, content_type=mimetype)
+    try:
+        vote = int(vote)
+    except ValueError:
+        vote = None
+    return vote_view(request, Entry, entry_id, vote,  mimetype='application/json')
+
