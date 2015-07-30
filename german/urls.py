5e903ff39b77ffe1574051430c89234453cd305e
eugena <eugena.mihailikova@gmail.com>
2015-07-30 19:29:36 +0500

    added urls

--- german/urls.py
+++ german/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from blog.views import like
+from blog.views import like, german_test
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^comments/', include('django_comments.urls')),
     url(r'^like/(?P<entry_id>\d+)/(?P<vote>\w+)', like),
-    url(r'^ckeditor/', include('ckeditor.urls')), )
+    url(r'^ckeditor/', include('ckeditor.urls')),
+    url(r'^feedback/', include('feedback.urls', namespace='feedback')),
+    url(r'^', include('zinnia.urls', namespace='zinnia')), )
